458de8caaaf75d9adfd4cf5a2c5b55a51b769b9b
micro-shop <xnovmir@seznam.cz>
2018-03-17 22:30:15 +0100

    fix

--- src/main/java/cz/microshop/users/controller/UsersController.java
+++ src/main/java/cz/microshop/users/controller/UsersController.java
@@ -24,6 +24,12 @@ public class UsersController {
     UsersService usersService;
 
     @RequestMapping(value = "/findAll", method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity<HttpStatus> deleteAll()   {
+        usersService.deleteAll();
+        return new ResponseEntity<HttpStatus>(HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/deleteAll", method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity<ArrayList<User>> findAll()   {
         return new ResponseEntity<ArrayList<User>>((ArrayList<User>) usersService.findAll(), HttpStatus.OK);
     }
