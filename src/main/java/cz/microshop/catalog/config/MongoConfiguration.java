090d7005cbc33eb14f9a6979ddd7437c32260a2d
micro-shop <xnovmir@seznam.cz>
2018-03-17 14:45:42 +0100

    fix

---
+++ src/main/java/cz/microshop/catalog/config/MongoConfiguration.java
@@ -0,0 +1,20 @@
+package cz.microshop.catalog.config;
+
+import com.mongodb.MongoClientOptions;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@AutoConfigureBefore(MongoAutoConfiguration.class)
+public class MongoConfiguration {
+
+    @Bean
+    public MongoClientOptions optionsProvider() {
+        MongoClientOptions.Builder optionsBuilder = new MongoClientOptions.Builder();
+        optionsBuilder.serverSelectionTimeout(10000);
+        MongoClientOptions options = optionsBuilder.build();
+        return options;
+    }
+}
